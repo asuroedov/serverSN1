c65fd6e1e8c6740aa7294be4a99fb739ff690d2e
asuroedov <a.suroedov@yandex.ru>
2021-02-26 01:30:57 +0300

    implemented some login logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,27 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/UserModel')
 
+const JWT_KEY = 'dev'
 
-module.exports.login = (req, res) => {
-    res.status(200).json({
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async (req, res) => {
+    const candidate = await User.findOne({login: req.body.login})
+    if(candidate){
+        if(bcrypt.compareSync(req.body.password, candidate.password)){
+            const token = jwt.sign({
+                login: candidate.login,
+                userId: candidate.userId
+            }, JWT_KEY, {expiresIn: 60 * 60})
+
+            res.status(200).json({resultCode: 0, message: '', data: {token: token}})
+        }else{
+            res.status(404).json({resultCode: 1, message: 'incorrect login or password'})
         }
-    )
+
+    } else {
+        res.status(404).json({resultCode: 1, message: 'incorrect login or password'})
+    }
+
 }
 
 module.exports.register = async (req, res) => {
