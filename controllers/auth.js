97ad30a45c066b4b6d7c84b1101a6dbd4948f02c
asuroedov <a.suroedov@yandex.ru>
2021-02-26 00:52:25 +0300

    implemented some registration logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/UserModel')
+
+
 module.exports.login = (req, res) => {
     res.status(200).json({
             email: req.body.email,
@@ -6,7 +10,28 @@ module.exports.login = (req, res) => {
     )
 }
 
-module.exports.register = (req, res) => {
-    res.status(200).json({register: '...'})
+module.exports.register = async (req, res) => {
+
+    const candidate = await User.findOne({login: req.body.login})
+
+    if (candidate) {
+        res.status(409).json({resultCode: 1, message: 'user with this login already exist '})
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+
+        const user = new User({
+            userId: Date.now(),
+            login: req.body.login,
+            password: bcrypt.hashSync(req.body.password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(200).json({resultCode: 0, message: '', temp: user})
+        } catch (e) {
+            res.status(400).json({resultCode: 1, message: 'some error'})
+        }
+    }
+
 }
 
