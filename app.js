b1de2d9df1ce6b7766c5779401f3d06c2bec1c73
asuroedov <a.suroedov@yandex.ru>
2021-02-25 22:29:18 +0300

    connect to mlab mongo db

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const authRoutes = require('./routes/auth')
 const bodyParser = require('body-parser')
 const app = express()
+
+mongoose.connect(`mongodb+srv://asuroedov:121212q@cluster0.wojtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('mongo db connected')
+    })
+    .catch(error => console.log(error))
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
