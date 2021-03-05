bc153ae3478687cc3308962b77da3ca7ffdbc738
asuroedov <a.suroedov@yandex.ru>
2021-03-05 13:50:24 +0300

    refactor getUsersList

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const UserController = require('../models/UserModel')
+const User = require('../models/UserModel')
 
 
 module.exports.getUsersList = async (req, res) => {
@@ -6,10 +6,11 @@ module.exports.getUsersList = async (req, res) => {
         let pageSize = +req.query.pageSize
         if(!pageSize || pageSize < 0 || pageSize > 10) pageSize = 5
 
-        const users = await UserController.find().sort({userId: -1}).limit(pageSize)
+        const totalCount = await User.countDocuments()
+        const users = await User.find().sort({userId: -1}).limit(pageSize)
         const result = []
-        users.forEach(el => result.push({login: el.login, name: el.name, shortName: el.shortName}))
-        res.status(200).json(result)
+        users.forEach(el => result.push({login: el.login, name: el.name, shortName: el.shortName, photoUrl: el.photoUrl}))
+        res.status(200).json({totalCount: totalCount, data: result})
 
     }catch (e) {
         res.status(400).json({resultCode: 1, message: 'getUsersList. some error', data: {}})
